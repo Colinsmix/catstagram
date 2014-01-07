d544553e109cd516f7ee09d4611c4c1381143a55
Eric Kelly <heroiceric@gmail.com>
2014-01-07 18:04:35 -0500

    Add seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+#
+
+cat_guy = User.find_or_initialize_by(email: "crazycatguy@launchacademy.co") do |user|
+  user.password = "w0lfd3n123"
+  user.password_confirmation = "w0lfd3n123"
+  user.save!
+end
+
+# file.open(file.join(rails.root, '/spec/support/fixtures/cats/sleeping_cat.jpeg'))
+
+Dir.glob("#{Rails.root}/spec/support/fixtures/cats/*.jpeg") do |picture|
+  cat_guy.posts.create(image: File.open(File.join(picture)))
+end
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -3,7 +3,7 @@
 FactoryGirl.define do
   factory :post do
     association :user
-    image File.open(File.join(Rails.root, '/spec/support/fixtures/sleeping_cat.jpeg'))
+    image File.open(File.join(Rails.root, '/spec/support/fixtures/cats/sleeping_cat.jpeg'))
 
     trait :with_description do
       sequence(:description) { |n| "Meet kitty#{n}. MEOWWW!" }
